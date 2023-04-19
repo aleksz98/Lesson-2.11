e9f278986cc036c150879efaeb01d68fd4aefe20
Aleksz <aleksznoob@gmail.com>
2023-04-19 13:32:53 +0200

    made mvc

---
+++ Lesson 2.11/ViewControllers/DetailPersonViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailPersonViewController.swift
+//  Lesson 2.11
+//
+//  Created by Иван on 19.04.2023.
+//
+
+import UIKit
+
+class DetailPersonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- Lesson 2.11/ViewControllers/PersonsViewController.swift
+++ Lesson 2.11/ViewControllers/PersonsViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class PersonsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
